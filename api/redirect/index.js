97b1fa72d3e2fe6778be0c0624e88d688f839bc9
Stuart Leeks <stuart@leeks.net>
2022-11-21 09:55:09 +0000

    Wildcard redirect route

--- api/redirect/index.js
+++ api/redirect/index.js
@@ -1,14 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
     context.res = {
-        status: 301,
-        body: responseMessage,
+        status: 302,
+        body: "Nothing to see here...",
         headers: {
             "Location": "https://stuartleeks.com"
         }
