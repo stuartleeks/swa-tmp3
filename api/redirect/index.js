af5a4a36b3ecc3444a23c86b79edbda5cd06351f
Stuart Leeks <stuart@leeks.net>
2022-11-21 14:14:12 +0000

    Send to service bus on redirect

--- api/redirect/index.js
+++ api/redirect/index.js
@@ -10,13 +10,17 @@ module.exports = async function (context, req) {
     context.log("Redirect function starting");
     context.log("Context: " + JSON.stringify(context));
 
-    const originalUrl = req.headers['x-ms-original-url'];
-    const originalPath = new URL(originalUrl).pathname
-    context.log("Original Path: " + originalPath);
-
-    const redirectUrl = redirects[originalPath] ? redirects[originalPath] : "https://wsl.tips";
-    context.log("Redirecting to: " + redirectUrl);
-
+    const originalPath = getOriginalPath(req, context);
+    const redirectUrl = getRedirectForPath(originalPath);
+    
+    // Send a message to service bus for later processing
+    context.log("Sending service bus message");
+    context.bindings.redirectQueue = {
+        path: originalPath,
+    };
+    
+    // redirect the client
+    context.log(`Redirecting to: ${redirectUrl}`);
     context.res = {
         status: 302,
         body: "Nothing to see here...",
@@ -30,4 +34,15 @@ module.exports = async function (context, req) {
             "Location": redirectUrl
         }
     };
+}
+
+function getRedirectForPath(originalPath) {
+    return redirects[originalPath] ? redirects[originalPath] : "https://wsl.tips";
+}
+
+function getOriginalPath(req, context) {
+    const originalUrl = req.headers['x-ms-original-url'];
+    const originalPath = new URL(originalUrl).pathname;
+    context.log("Original Path: " + originalPath);
+    return originalPath;
 }
