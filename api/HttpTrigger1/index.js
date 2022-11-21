bb9df9d4ca411035ff53f0bc44217f0e89c62c5f
Stuart Leeks <stuart@leeks.net>
2022-11-21 10:52:48 +0000

    Add redirect dictionary

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -3,5 +3,6 @@ module.exports = async function (context, req) {
 
     context.res.json({
         text: "Hello from the API"
+        ,res: JSON.stringify(context.res)
     });
 }
--- api/redirect/index.js
+++ api/redirect/index.js
@@ -1,11 +1,28 @@
+
+const redirects = {
+    "/api/test2": "https://stuartleeks.com",
+    "/stuartleeks": "https://stuartleeks.com",
+    "/microsoft": "https://microsoft.com",
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    const originalUrl = req.headers['x-ms-original-url'];
+    const originalPath = new URL(originalUrl).pathname
+
+    const redirectUrl = redirects[originalPath] ?? "https://wsl.tips";
 
     context.res = {
         status: 302,
         body: "Nothing to see here...",
+        // body: JSON.stringify({
+        //     ctx: context,
+        //     originalUrl,
+        //     originalPath
+        // }),
         headers: {
-            "Location": "https://stuartleeks.com"
+            "Location": redirectUrl
         }
     };
 }
