2e075b68b165d7ec6e704d2deb4167e3d63b2735
Stuart Leeks <stuart@leeks.net>
2022-11-21 20:31:56 +0000

    Add basic signalr

--- back-end/ServiceBusQueueTrigger/index.ts
+++ back-end/ServiceBusQueueTrigger/index.ts
@@ -4,25 +4,29 @@ import * as appInsights from "applicationinsights";
 appInsights.setup();
 const client = appInsights.defaultClient;
 
-const serviceBusQueueTrigger: AzureFunction = async function (context: Context, redirectMessage: { path: string }): Promise<void> {
+const serviceBusQueueTrigger: AzureFunction = async function (
+    context: Context,
+    redirectMessage: { path: string },
+    signalRMessages: { target: string, arguments: any[] }[]
+): Promise<void> {
     context.log(`Process service bus message. Path=${redirectMessage.path}`);
 
     // Use this with 'tagOverrides' to correlate custom telemetry to the parent function invocation.
+    context.log(`Write custom metric to Application Insights`);
     var operationIdOverride = { "ai.operation.id": context.traceContext.traceparent };
-
     client.trackMetric({
         name: "redirect",
         value: 1,
         properties: { path: redirectMessage.path },
         tagOverrides: operationIdOverride
     });
-    client.trackEvent({
-        name: "redirect",
-        properties: { path: redirectMessage.path },
-        tagOverrides: operationIdOverride
-    });
-
     client.flush(); // demo aid - don't do this!
+
+    context.log(`Send SignalR message`);
+    context.bindings.signalRMessages = [{
+        target: "redirect",
+        arguments: [redirectMessage.path]
+    }];
 };
 
 export default serviceBusQueueTrigger;
