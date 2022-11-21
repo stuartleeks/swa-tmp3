12acbe6ffcbedd8bc8cac98accaa2929d31e56e1
Stuart Leeks <stuart@leeks.net>
2022-11-21 16:38:28 +0000

    Add custom metric

--- back-end/ServiceBusQueueTrigger/index.ts
+++ back-end/ServiceBusQueueTrigger/index.ts
@@ -1,7 +1,28 @@
 import { AzureFunction, Context } from "@azure/functions"
+import * as appInsights from "applicationinsights";
 
-const serviceBusQueueTrigger: AzureFunction = async function(context: Context, redirectMessage: {path:string}): Promise<void> {
+appInsights.setup();
+const client = appInsights.defaultClient;
+
+const serviceBusQueueTrigger: AzureFunction = async function (context: Context, redirectMessage: { path: string }): Promise<void> {
     context.log(`Process service bus message. Path=${redirectMessage.path}`);
+
+    // Use this with 'tagOverrides' to correlate custom telemetry to the parent function invocation.
+    var operationIdOverride = { "ai.operation.id": context.traceContext.traceparent };
+
+    client.trackMetric({
+        name: "redirect",
+        value: 1,
+        properties: { path: redirectMessage.path },
+        tagOverrides: operationIdOverride
+    });
+    client.trackEvent({
+        name: "redirect",
+        properties: { path: redirectMessage.path },
+        tagOverrides: operationIdOverride
+    });
+
+    client.flush(); // demo aid - don't do this!
 };
 
 export default serviceBusQueueTrigger;
