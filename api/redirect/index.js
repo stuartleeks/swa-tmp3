f8f2a19abda3b4999e3ea227c273376ff541a29f
Stuart Leeks <stuart@leeks.net>
2022-11-23 10:24:13 +0000

    Simplify code

--- api/redirect/index.js
+++ api/redirect/index.js
@@ -5,12 +5,14 @@ const redirects = {
     "/microsoft": "https://microsoft.com",
 }
 
+
+
 module.exports = async function (context, req) {
 
     context.log("Redirect function starting");
     context.log("Context: " + JSON.stringify(context));
 
-    const originalPath = getOriginalPath(req, context);
+    const originalPath = getOriginalPath(req);
     const redirectUrl = getRedirectForPath(originalPath);
     
     // Send a message to service bus for later processing
@@ -25,25 +27,17 @@ module.exports = async function (context, req) {
     context.res = {
         status: 302,
         body: "Nothing to see here...",
-        // body: JSON.stringify({
-        //     ctx: context,
-        //     originalUrl,
-        //     originalPath,
-        //     redirectUrl
-        // }),
         headers: {
             "Location": redirectUrl
         }
     };
 }
 
-function getRedirectForPath(originalPath) {
-    return redirects[originalPath] ?? "https://wsl.tips";
+function getOriginalPath(req) {
+    const originalUrl = req.headers['x-ms-original-url'];
+    return new URL(originalUrl).pathname;
 }
 
-function getOriginalPath(req, context) {
-    const originalUrl = req.headers['x-ms-original-url'];
-    const originalPath = new URL(originalUrl).pathname;
-    context.log("Original Path: " + originalPath);
-    return originalPath;
+function getRedirectForPath(originalPath) {
+    return redirects[originalPath] ?? "https://wsl.tips";
 }
